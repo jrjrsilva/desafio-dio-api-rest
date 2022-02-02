9b5f7817d77215cf57536aeafb7c343d095db074
José R S Santos <jrjrsilva@gmail.com>
2022-02-02 13:59:55 -0300

    Ajustes AddressDTO

--- src/main/java/one/digitalinnovation/personapi/dto/request/AddressDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/AddressDTO.java
@@ -1,9 +1,18 @@
 package one.digitalinnovation.personapi.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddressDTO {
 
     private Long id;
